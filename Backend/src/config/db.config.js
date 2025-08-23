a040071aa5132f79543b510fdff6838c9989f20e
mohanlal99 <mohanlalv433@gmail.com>
2025-08-23 18:33:09 +0530

    Update db connection

--- Backend/src/config/db.config.js
+++ Backend/src/config/db.config.js
@@ -10,8 +10,8 @@ export const connectToDB = async () => {
   if (isConnected) return;
 
   try {
-    console.log("mongodb+srv://mohanlal:Mohanlal%404321%23@cluster0.t4mcf4u.mongodb.net/insurance" || "mongodb+srv://mohanlal:Mohanlal%404321%23@cluster0.t4mcf4u.mongodb.net/insurance")
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/');
+    // console.log("mongodb+srv://mohanlal:Mohanlal%404321%23@cluster0.t4mcf4u.mongodb.net/insurance" || "mongodb+srv://mohanlal:Mohanlal%404321%23@cluster0.t4mcf4u.mongodb.net/insurance")
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mohanlal:Mohanlal%404321%23@cluster0.t4mcf4u.mongodb.net/insurance');
     isConnected = true;
     console.log("MongoDB connected");
   } catch (err) {
