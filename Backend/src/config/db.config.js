b5394739aa063c315279e8a90605caa0cb7d623e
mohanlal99 <mohanlalv433@gmail.com>
2025-08-23 16:06:11 +0530

    Mongodb uri update

--- Backend/src/config/db.config.js
+++ Backend/src/config/db.config.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 
-export async function connectToDB() {
+let isConnected = false;
+
+export const connectToDB = async () => {
+  if (isConnected) return;
+
   try {
-    console.log("🔍 Connecting to MongoDB...");
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB connected");
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
-    process.exit(1);
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("DB connection failed", err);
   }
-}
+};
